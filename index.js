adb7f25f5ec38de21a5eaf063a961fd0eff1e7a2
ahmedkatan410 <ahmedkatan410@gmail.com>
2022-10-06 22:08:36 +0300

    3

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
 
-export const InputText = (props) => {
-  const { name, onChange, placeholder, required, id } = props;
+export const ErTextField = (props) => {
+  const { name, label, onChange, placeholder, required, id, IconStart, size } =
+    props;
   return (
-    <div className="form-group">
-      <input
-        type="text"
-        className="form-control"
-        id={`${id}`}
-        name={`${id}`}
-        value={name}
+    <div>
+      <TextField
+        label={label}
+        id={name}
+        sx={{ m: 0.1, width: "30ch" }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">{IconStart}</InputAdornment>
+          ),
+        }}
+        name="userName"
+        // // value={userName}
         onChange={onChange}
-        placeholder={`${placeholder}`}
-        required={required}
+        placeholder={placeholder}
+        required
+        size={size}
       />
     </div>
   );
 };
-
-module.exports = InputText;

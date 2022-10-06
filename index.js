5001769321f355bfba9e975c0c745643f0f547c2
ahmedkatan410 <ahmedkatan410@gmail.com>
2022-10-06 22:34:26 +0300

    4

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ export const ErTextField = (props) => {
   const { name, label, onChange, placeholder, required, id, IconStart, size } =
     props;
   return (
-    <div>
-      <TextField
-        label={label}
-        id={name}
-        sx={{ m: 0.1, width: "30ch" }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">{IconStart}</InputAdornment>
-          ),
-        }}
-        name="userName"
-        // // value={userName}
-        onChange={onChange}
-        placeholder={placeholder}
-        required
-        size={size}
-      />
-    </div>
+    <TextField
+      label={label}
+      id={name}
+      sx={{ m: 0.1, width: "30ch" }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">{IconStart}</InputAdornment>
+        ),
+      }}
+      name="userName"
+      // // value={userName}
+      onChange={onChange}
+      placeholder={placeholder}
+      required
+      size={size}
+    />
   );
 };
